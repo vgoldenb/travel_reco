9dbe8dcba264a249b32cde0365ec7501292252e1
vgoldenb <vitgold@yahoo.com>
2025-07-31 17:41:39 -0400

    final adjustments

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -1,37 +1,50 @@
 const btnSearch = document.getElementById('btnSearch');
+const btnClear = document.getElementById('btnClear');
 const resultDiv = document.getElementById('result');
 
 function searchDestination() {
-const input = document.getElementById('searchInput').value.toLowerCase();
-resultDiv.innerHTML = '';
+    const input = document.getElementById('searchInput').value.toLowerCase();
+    resultDiv.innerHTML = '';
 
-fetch('travel_recommendation_api.json')
-    .then(response => response.json())
-    .then(data => {
-    if (input=='country' || input=='countries') {
-        data.countries.forEach (function(country) {
-            displayDestination(country.cities);
+    fetch('travel_recommendation_api.json')
+        .then(response => response.json())
+        .then(data => {
+            const country = data.countries.find(item => item.name.toLowerCase() === input);
+
+            if (country) {
+                displayDestination(country.cities);
+            } else if (input=='country' || input=='countries') {
+                data.countries.forEach (function(country) {
+                    displayDestination(country.cities);
+                });
+            } else if (input=='beach' || input=='beaches') {
+                displayDestination(data.beaches);
+            } else if (input=='temple' || input=='temples') {
+                displayDestination(data.temples);
+            } else {
+                    resultDiv.innerHTML = 'Result not found';
+            }
+        })
+        .catch(error => {
+        console.error('Error:', error);
+        resultDiv.innerHTML = 'An error occurred while fetching data.';
         });
-    } else if (input=='beach' || input=='beaches') {
-        displayDestination(data.beaches);
-    } else if (input=='temple' || input=='temples') {
-        displayDestination(data.temples);
-    } else {
-            resultDiv.innerHTML = 'Result not found';
-    }
-    })
-    .catch(error => {
-    console.error('Error:', error);
-    resultDiv.innerHTML = 'An error occurred while fetching data.';
-    });
 }
 
-  function displayDestination(locationType) {
+function displayDestination(locationType) {
     locationType.forEach (function(location) {
-        resultDiv.innerHTML += `<h4>${location.name}</h4>`;
+        const localTime = new Date().toLocaleTimeString('en-US', { timeZone: location.timeZone, hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' });
+        
+        resultDiv.innerHTML += `<h4>${location.name}</h4> (local time: ${localTime})`;
         resultDiv.innerHTML += `<img src="${location.imageUrl}">`;
         resultDiv.innerHTML += `<p>${location.description}</p>`;
     });
-  }
+}
 
-  btnSearch.addEventListener('click', searchDestination);
+function clearResult() {
+    document.getElementById('searchInput').value = '';
+    resultDiv.innerHTML = '';
+}
+
+btnSearch.addEventListener('click', searchDestination);
+btnClear.addEventListener('click', clearResult);
