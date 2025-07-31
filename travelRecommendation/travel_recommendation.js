94a5a074b20ce17cf82b8504a87701ceb50f059b
vgoldenb <vitgold@yahoo.com>
2025-07-31 16:41:15 -0400

    Keyword searches

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -1,30 +1,37 @@
 const btnSearch = document.getElementById('btnSearch');
+const resultDiv = document.getElementById('result');
 
+function searchDestination() {
+const input = document.getElementById('searchInput').value.toLowerCase();
+resultDiv.innerHTML = '';
 
-  function searchDestination() {
-    const input = document.getElementById('searchInput').value.toLowerCase();
-    const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = '';
+fetch('travel_recommendation_api.json')
+    .then(response => response.json())
+    .then(data => {
+    if (input=='country' || input=='countries') {
+        data.countries.forEach (function(country) {
+            displayDestination(country.cities);
+        });
+    } else if (input=='beach' || input=='beaches') {
+        displayDestination(data.beaches);
+    } else if (input=='temple' || input=='temples') {
+        displayDestination(data.temples);
+    } else {
+            resultDiv.innerHTML = 'Result not found';
+    }
+    })
+    .catch(error => {
+    console.error('Error:', error);
+    resultDiv.innerHTML = 'An error occurred while fetching data.';
+    });
+}
 
-    fetch('travel_recommendation_api.json')
-      .then(response => response.json())
-      .then(data => {
-        const country = data.countries.find(item => item.name.toLowerCase() === input);
-
-        if (country) {
-            country.cities.forEach (function(city) {
-                resultDiv.innerHTML += `<h4>${city.name}</h4>`;
-                resultDiv.innerHTML += `<img src="${city.imageUrl}">`;
-                resultDiv.innerHTML += `<p>${city.description}</p>`;
-            });
-        } else {
-          resultDiv.innerHTML = 'country not found.';
-        }
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        resultDiv.innerHTML = 'An error occurred while fetching data.';
-      });
+  function displayDestination(locationType) {
+    locationType.forEach (function(location) {
+        resultDiv.innerHTML += `<h4>${location.name}</h4>`;
+        resultDiv.innerHTML += `<img src="${location.imageUrl}">`;
+        resultDiv.innerHTML += `<p>${location.description}</p>`;
+    });
   }
 
   btnSearch.addEventListener('click', searchDestination);
